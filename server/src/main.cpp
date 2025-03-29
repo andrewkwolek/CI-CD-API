410d19f6ace1658dde8eb4913863106c0de0450c
Andrew Kwolek <andrewkwolek2025@u.northwestern.edu>
2025-03-29 18:56:07 -0500

    Added working c++ code

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -5,13 +5,62 @@
 #include <grpcpp/server_context.h>
 
 #include <string>
+#include <map>
+
+#include "data_transfer.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+
+using data_transfer::Item;
+using data_transfer::ItemTransfer;
+
+class ItemTransferImpl final : public ItemTransfer::Service {
+public:
+    ItemTransferImpl() {}
+
+    Status GetItem(ServerContext* context, const Item* request, Item* response) override {
+        int id = request->id();
+        std::string name = request->name();
+    
+
+        if (table.find(request->id()) != table.end()) {
+            *response = *request;
+            return Status::OK;
+        }
+        else {
+            return Status(grpc::StatusCode::NOT_FOUND, "Item not found");
+        }
+    }
+
+    Status SetItem(ServerContext* context, const Item* request, Item* response) override {
+        table[request->id()] = request->name();
+        *response = *request;
+
+        return Status::OK;
+    }
+
+private:
+    std::unordered_map<int, std::string> table;
+};
 
 void RunServer() {
     std::string server_address("0.0.0.0:8082");
+    ItemTransferImpl service;
 
     grpc::ServerBuilder builder;
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+
+    builder.RegisterService(&service);
+
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+
+    server->Wait();
 }
 
-int main(char* argc, char** argv) {
+int main(int argc, char** argv) {
+    RunServer();
     return 0;
 }
